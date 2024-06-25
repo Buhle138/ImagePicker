5eb096b2151799540a9bc73a0dc6ff5997df58b0
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-25 13:38:31 +0200

    Initial Commit

---
+++ ImagePicker/ImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerApp.swift
+//  ImagePicker
+//
+//  Created by Buhle Radzilani on 2024/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
